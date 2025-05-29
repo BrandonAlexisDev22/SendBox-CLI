1d921da9fc88ec72648f0c2badd785f2bafa9a63
BrandonAlexisDev22 <brandonalexisquintero.sena@gmail.com>
2025-05-28 19:34:57 -0500

    Implement saveData function to store email and token in .env file

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ const nodemailer = require("nodemailer");
 const inquirer = require("inquirer");
 const prompt = inquirer.createPromptModule();
 const fs = require("node:fs");
+
+function saveData(email, token) {
+  const envContent = `EMAIL_USER=${email}\nTOKEN_USER=${token}`;
+  fs.writeFileSync("../.env", envContent);
+  console.log("Datos guardados correctamente");
+  require("dotenv").config({ path: "../.env" });
+}
